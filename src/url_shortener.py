4836b79e96631fa72f88f87d196846aa212c89e9
Ilhomidin Bahoraliev <itilhomidin@yandex.com>
2021-03-13 05:15:54 +0500

    Add url_shortener module

---
+++ src/url_shortener.py
@@ -0,0 +1,68 @@
+from typing import Union
+
+import urllib3
+import orjson
+
+
+def _api_call(api_key: str, url: str) -> urllib3.HTTPResponse:
+    """Make API call to cutt.ly
+
+    Args:
+        api_key (str): user API key
+        url (str): URL you want to shorten
+
+    Returns:
+        urllib3.HTTPResponse: urrlib repsponse
+    """
+    http = urllib3.PoolManager()
+    return http.request(
+        "GET" f"http://cutt.ly/api/api.php?key={api_key}&short={url}",
+    )
+
+
+def _parse_json(raw: Union[str, bytes, bytearray]) -> dict:
+    """Parse json
+
+    Args:
+        raw (Union[str, bytes, bytearray]): data
+
+    Returns:
+        dict: serialized json
+    """
+    try:
+        json = orjson.loads(raw)
+    except orjson.JSONDecodeError:
+        json = {"url": {"status": 8}}
+    return json
+
+
+def shorten(api_key: str, url: str) -> str:
+    """Try to make given url shorten
+
+    Args:
+        api_key (str): user API key
+        url (str): URL you want to shorten
+
+    Returns:
+        str: message or url
+    """
+    api_response = _parse_json(_api_call(api_key, url).data)
+    if status := api_response["url"]["status"] == 1:
+        return url
+    elif status == 2:
+        return f"{url} is not a link"
+    elif status == 5:
+        return (
+            f"{url} has not passed the validation. Includes invalid characters"
+        )
+    elif status == 6:
+        return f"{url} provided is from a blocked domain"
+    elif status == 7:
+        return api_response["url"]["shortLink"]
+    elif status == 8:
+        return f"Can't make {url} shorten"
+    else:
+        return f"Bot Error"
+
+
+__all__ = ["shorten"]
