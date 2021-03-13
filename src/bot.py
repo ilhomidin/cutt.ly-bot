3054836903959bde7e5e58c0b4deea7d7b1b0af0
Ilhomidin Bahoraliev <itilhomidin@yandex.com>
2021-03-13 05:58:34 +0500

    Add handler for message with text content type

--- src/bot.py
+++ src/bot.py
@@ -9,11 +9,15 @@ bot = telebot.TeleBot(os.environ["TELEGRAM_BOT_TOKEN"], parse_mode=None)
 
 
 def shorten(url: str) -> str:
+    """Partial url_shortener.shorten function
+    Load api_key from environ
+    """
     return url_shortener.shorten(os.environ["CUTTLY_API_KEY"], url)
 
 
 @bot.message_handler(content_types=["text"])
 def short_link(message: telebot.types.Message):
+    """Shorten given link"""
     answer = shorten(message.text)
     bot.reply_to(message, answer)
 
